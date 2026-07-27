e08270dbd58dc1e906e2b50d354b21b46cd6a5e6
Sunil Gautam <gautamsunil917@gmail.com>
2026-07-27 14:08:30 +0545

    completed Shopnow Page

--- frontend/components/product.js
+++ frontend/components/product.js
@@ -1,5 +1,5 @@
 export function productCard(image, category, title, tags, description, price) {
-    return `
+  return `
   <div class="CardConatiner">
     <div class="imageDiv">
       <img src=${image} alt="" />
@@ -25,51 +25,76 @@ export function productCard(image, category, title, tags, description, price) {
   </div>
     `
 
-    CardConatiner
+  CardConatiner
 }
 
 function renderTags(tags = []) {
-    return (
-        tags.map((tag) => (
-            `<span class="tag">${tag}</span>`
-        )).join("")
-    )
+  return (
+    tags.map((tag) => (
+      `<span class="tag">${tag}</span>`
+    )).join("")
+  )
 }
 
 
 
 
 export const products = [
-    {
-        image: "./images/image.png",
-        category: "Pizza",
-        title: "Pepperoni Pizza",
-        tags: ["Cheesy", "Hot", "Popular"],
-        description: "Freshly baked pepperoni pizza with extra cheese and crispy crust.",
-        price: "$12.99"
-    },
-    {
-        image: "./images/image.png",
-        category: "Burger",
-        title: "Chicken Burger",
-        tags: ["Spicy", "Grilled"],
-        description: "Juicy grilled chicken burger served with fresh lettuce and cheese.",
-        price: "$8.49"
-    },
-    {
-        image: "./images/image.png",
-        category: "Dessert",
-        title: "Chocolate Cake",
-        tags: ["Sweet", "Best Seller"],
-        description: "Rich chocolate cake topped with creamy chocolate frosting.",
-        price: "$6.99"
-    },
-    {
-        image: "./images/image.png",
-        category: "Dessert",
-        title: "Chocolate Cake",
-        tags: ["Sweet", "Best Seller"],
-        description: "Rich chocolate cake topped with creamy chocolate frosting.",
-        price: "$6.99"
-    },
+  {
+    image: "./images/image.png",
+    category: "Pizza",
+    title: "Pepperoni Pizza",
+    tags: ["Cheesy", "Hot", "Popular"],
+    description: "Freshly baked pepperoni pizza with extra cheese and crispy crust.",
+    price: "$12.99"
+  },
+  {
+    image: "./images/image.png",
+    category: "Burger",
+    title: "Chicken Burger",
+    tags: ["Spicy", "Grilled"],
+    description: "Juicy grilled chicken burger served with fresh lettuce and cheese.",
+    price: "$8.49"
+  },
+  {
+    image: "./images/image.png",
+    category: "Dessert",
+    title: "Chocolate Cake",
+    tags: ["Sweet", "Best Seller"],
+    description: "Rich chocolate cake topped with creamy chocolate frosting.",
+    price: "$6.99"
+  },
+  {
+    image: "./images/image.png",
+    category: "Dessert",
+    title: "Chocolate Cake",
+    tags: ["Sweet", "Best Seller"],
+    description: "Rich chocolate cake topped with creamy chocolate frosting.",
+    price: "$6.99"
+  },
 ];
+
+
+export const category = [
+  {
+    name: "Shoes",
+    color: "red"
+  },
+  {
+    name: "Shoes",
+    color: "red"
+  },
+  {
+    name: "Shoes",
+    color: "red"
+  },
+]
+
+export const size = [
+  "S",
+  "M",
+  "L",
+  "XL",
+  "2XL",
+  "3XL"
+]
--- frontend/scripts/shop.js
+++ frontend/scripts/shop.js
@@ -1,10 +1,9 @@
 import { productCard } from "../components/product.js";
 import { products } from "../components/product.js";
-
+import { category } from "../components/product.js";
+import { size } from "../components/product.js";
 
 const cards = document.querySelector('.cards')
-
-
 
 let card = ""
 products.forEach((item) => (
@@ -18,6 +17,57 @@ products.forEach((item) => (
     )))
 
 cards.innerHTML = card
+
+const catogeryFilter = document.querySelector('.catogeryFilter')
+const categoryFilterCon = document.querySelector('#category-Filter')
+
+let categoryHtml = ""
+
+category.forEach((item) => {
+    categoryHtml += `
+      <div class="item">
+       <input type="checkbox" />
+       <p>${item.name}</p>
+       </div>
+    `
+})
+catogeryFilter.innerHTML = categoryHtml
+categoryFilterCon.innerHTML = categoryHtml
+
+
+const sizeItems = document.querySelector('.sizeItems')
+const sizeItemsCon = document.querySelector('#size-Items')
+
+let sizeHtml = ""
+
+size.forEach((item) => {
+    sizeHtml += `
+    <span class="">
+     ${item}
+     </span>
+    `
+})
+sizeItems.innerHTML = sizeHtml
+sizeItemsCon.innerHTML = sizeHtml
+
+const filterClose = document.querySelector('.closeBtn')
+const leftSideMobile = document.querySelector('.leftSideMobile')
+
+filterClose.addEventListener('click', () => {
+    leftSideMobile.style.display = 'none'
+})
+
+const filterOpen = document.querySelector('.filterOpen')
+
+filterOpen.addEventListener('click', () => {
+    leftSideMobile.style.display = 'block'
+})
+
+
+
+
+
+
 
 
 
