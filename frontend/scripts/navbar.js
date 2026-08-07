ad167210e8d2130d8ca6a3c18ae5404653a88aba
Sunil Gautam <gautamsunil917@gmail.com>
2026-08-07 13:20:29 +0545

    Navbar fixed as well

--- frontend/scripts/navbar.js
+++ frontend/scripts/navbar.js
@@ -1,19 +1,8 @@
+const isAuthenticated = false
 
-let html = `<div class="navbarContainer container">
-      <div class="logo" id="logo">Jhutta Bajar </div>
+function renderNavbar() {
 
-      <nav>
-        <a href="/index.html">Home</a>
-        <a href="/shop.html">Shop Now</a>
-        <a href="/cart.html">Cart</a>
-        <a href="/order.html">Order</a>
-      </nav>
-
-      <div class="navbarButton hidecontainer">
-        <button>
-          Get Started
-        </button>
-      </div>
+  let login = `
       <div class="loggedIn ">
         <div class="btn">
           <i class="fa-solid fa-circle-user" style="font-size: 27px"></i>
@@ -30,55 +19,103 @@ let html = `<div class="navbarContainer container">
           </div>
         </div>
       </div>
+  `
+
+  let withoutLogin = `
+    <div class="navbarButton ">
+        <button id="getStarted">
+          Get Started
+        </button>
+      </div>
+  `
+
+  let html = `<div class="navbarContainer container">
+      <div class="logo" id="logo">Jhutta Bajar </div>
+
+      <nav>
+        <a href="/index.html">Home</a>
+        <a href="/shop.html">Shop Now</a>
+        <a href="/cart.html">Cart</a>
+        <a href="/order.html">Order</a>
+      </nav>
+
+      ${isAuthenticated ?
+      login
+      : withoutLogin
+    }
+
+    
       <div class="mobileViewbutton">
         <i class="fa-solid fa-bars" style="font-size: 27px;"></i>
       </div>
     </div>
     <div class="mobileview">
       <nav>
-        <a href="">Home</a>
-        <a href="">Shop Now</a>
-        <a href="">Cart</a>
-        <a href="">Order</a>
+         <a href="/index.html">Home</a>
+        <a href="/shop.html">Shop Now</a>
+        <a href="/cart.html">Cart</a>
+        <a href="/order.html">Order</a>
       </nav>
 
     </div>`
 
-const navBarContainer = document.querySelector('#navBarContainer')
+  const navBarContainer = document.querySelector('#navBarContainer')
 
-navBarContainer.innerHTML = html
+  navBarContainer.innerHTML = html
+
+}
+
+renderNavbar()
+
+
+
 
 const profileBtn = document.querySelector('.btn')
 const profileDropdown = document.querySelector('.profileDropdown')
 
-let isOpen = false
-profileBtn.addEventListener('click', () => {
+if (profileBtn) {
+
+  let isOpen = false
+  profileBtn.addEventListener('click', () => {
     if (isOpen) {
-        profileDropdown.style.display = 'none'
-        isOpen = false
+      profileDropdown.style.display = 'none'
+      isOpen = false
     } else {
-        profileDropdown.style.display = 'flex'
-        isOpen = true
+      profileDropdown.style.display = 'flex'
+      isOpen = true
     }
-})
+  })
+}
 
 const mobileViewNavbarButton = document.querySelector('.mobileViewbutton')
 const mobileViewNavbar = document.querySelector('.mobileview')
-let isNavOpen = false
-mobileViewNavbarButton.addEventListener('click', () => {
-    if (isNavOpen) {
-        mobileViewNavbar.style.display = 'none'
-        isNavOpen = false
-    } else {
-        mobileViewNavbar.style.display = "flex"
-        isNavOpen = true
-    }
 
-})
+if (mobileViewNavbarButton) {
+  let isNavOpen = false
+  mobileViewNavbarButton.addEventListener('click', () => {
+    if (isNavOpen) {
+      mobileViewNavbar.style.display = 'none'
+      isNavOpen = false
+    } else {
+      mobileViewNavbar.style.display = "flex"
+      isNavOpen = true
+    }
+  })
+}
 
 const logo = document.querySelector('#logo')
 
-logo.addEventListener('click', () => {
+if (logo) {
+  logo.addEventListener('click', () => {
     window.location.href = '/index.html'
-})
+  })
+}
+
+const getStarted = document.querySelector('#getStarted')
+
+if(getStarted){
+  getStarted.addEventListener('click',()=>{
+    window.location.href = '/auth.html'
+  })
+}
 
