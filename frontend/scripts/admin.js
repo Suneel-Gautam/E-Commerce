a1d0d24ca2a87ee65d884bf40f3ca8a928baca81
Sunil Gautam <gautamsunil917@gmail.com>
2026-09-01 22:43:59 +0545

    updated code

--- frontend/scripts/admin.js
+++ frontend/scripts/admin.js
@@ -1,7 +1,25 @@
 const sliderButton = document.querySelector('#sliderButton')
 const leftSide = document.querySelector('#leftSide')
-
+const closebutton = document.querySelector("#closebutton")
 
 sliderButton.addEventListener('click', () => {
     leftSide.style.display = 'block'
+    sliderButton.style.display = 'none'
 })
+closebutton.addEventListener('click', () => {
+    leftSide.style.display = 'none'
+    sliderButton.style.display = 'flex'
+})
+
+const addUserConatiner = document.querySelector('#addUserModal')
+const addUserButton = document.querySelector('#addUserButton')
+const modalCloseButton = document.querySelector('#modalCloseButton')
+
+addUserButton.addEventListener('click', () => {
+    addUserConatiner.style.display = "flex"
+})
+modalCloseButton.addEventListener('click', () => {
+    addUserConatiner.style.display = "none"
+
+})
+
