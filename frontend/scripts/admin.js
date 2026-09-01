bce2a56e38a728b7018dfeec0f136ebd42f69b5b
Sunil Gautam <gautamsunil917@gmail.com>
2026-09-01 20:46:36 +0545

    updated code

---
+++ frontend/scripts/admin.js
@@ -0,0 +1,7 @@
+const sliderButton = document.querySelector('#sliderButton')
+const leftSide = document.querySelector('#leftSide')
+
+
+sliderButton.addEventListener('click', () => {
+    leftSide.style.display = 'block'
+})
